6d622cd624f47a55b796f659dddc6b5b667f49d1
Jonathan Cohen <jcohen4@swarthmore.edu>
2016-11-01 15:48:34 -0400

    Major cleanup to create new SWMMWise repository 11.2.16

--- call_test.py
+++
@@ -1,5 +0,0 @@
-'''#!/usr/bin/python'''
-import subprocess
-#subprocess.call(["ls","-l"], stdout=None,stderr=None,shell=True)
-#subprocess.check_output(['ls','-l'])
-output = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE).communicate()[0]
