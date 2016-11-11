cf21ce6425a4dad14b69ef61b356398dc127518f
Jonathan Cohen <jcohen4@swarthmore.edu>
2016-11-11 11:18:38 -0500

    filed for LIDs in PWD wingo initial add

---
+++ swmm_run_cso.py
@@ -0,0 +1,48 @@
+from swmm_objects import *
+from swmm_read_cso import *
+from pymongo import MongoClient
+from datetime import datetime
+import yaml
+from subprocess import call
+import sys
+
+def runswmm(runParamList,swmmInitialInpFileStr,runsCollection):
+
+    (section_names,sections) = read_inp(swmmInitialInpFileStr)
+    # Create an instance of a swmm_model object using the SWMM inp data:
+    model1 = swmm_model('Model1',section_names,sections)
+    #print sections['[INFILTRATION]']
+    for item in runParamList:
+        subCat = item['Subcat']
+        lid = item['LID']
+        newNumber = item['Number']
+        newArea = item['Area']
+        newWidth = item['Width']
+        CapRatioPct = item['CapRatioPct']
+        model1.lidChangeNumber(subCat,lid,newNumber)
+        model1.lidChangeArea(subCat,lid,newArea,CapRatioPct)
+        ###model1.lidChangeWidth(subCat,lid,newWidth)
+        ###model1.lidChangeFromImp(subCat,lid,newFromImp)
+    # Write out the model to a new file because SWMM must have a file
+    f = open("SWMM_modified.inp",'w')
+    swmmInputFileStr=model1.output()
+    f.write(swmmInputFileStr)  # write out the swmmInputFileStr for modified problem
+    f.close()
+    # Run the new model file
+    startTime = datetime.now()   # obtain the starting time of the run
+    startTimeStr = str(startTime)
+    call(["swmm5","SWMM_modified.inp", "SWMM_modified.txt", "out.out"])
+    endTime = datetime.now()   # obtain the ending time of the run
+    elapsedTime = endTime - startTime
+    minAndSec = divmod(elapsedTime.total_seconds(), 60)
+    elapsedTimeStr = "%s min, %0.2f sec" % minAndSec
+    print elapsedTimeStr
+
+    # collect the new results:
+    (peak,volume,cso_volume,runoff,evaporation,infiltration,precipitation,lid_dict) = read_report("SWMM_modified.txt", 0.1)
+    run = {"peak": peak, "volume": volume, "cso_volume": cso_volume, "runoff": runoff, "evaporation": evaporation, \
+    "infiltration": infiltration, "precipitation": precipitation, "lidDict": lid_dict, "swmmInputFileStr": swmmInputFileStr, \
+    "runParamList": runParamList, "swmmStartTime": startTimeStr,"swmmRunTime": elapsedTimeStr}
+    doc_id = runsCollection.insert_one(run).inserted_id
+    print "volume = %s" % volume
+    return (doc_id)
