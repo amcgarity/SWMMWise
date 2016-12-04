be3f9ba65181b20d68879fa8141d25dc755b7b94
jcohen4 <jcohen4@swarthmore.edu>
2016-12-04 16:19:31 -0500

    files with functions for the multivariate regression

---
+++ get_cso.py
@@ -0,0 +1,40 @@
+
+def calculate_cso(outflow_values, ratio):
+    cso_flow = 0
+    hours = 0
+    tot_flow = 0
+    max_treatment = 3122*ratio #max cfs allowed to treatment
+    #print max_treatment
+    tot = len(outflow_values)
+    for i in outflow_values: #out_variables is list within list (though outer list is just one element) (cfs/impervious acres)
+        tot_flow += i
+        if i > max_treatment:  #ratio method--- 
+            cso = i - max_treatment # subtracting treated from total outflow
+            cso_flow += cso 
+            hours += 1
+    tot_volume = tot_flow*900*7.48052 #convert to gallons, 900 seconds in 15 minutes
+    cso_volume = cso_flow*900*7.48052 #for seconds in 15 minutes
+    #equiv_rat = cso_volume/tot_volume  #equivalency ratio
+    treated_volume = tot_volume - cso_volume
+    #print tot_volume
+    return {"cso_volume":cso_volume, "tot_volume":tot_volume}
+
+def cso_reduction(collectionName, ratio, numSubs, results): 
+    from process_collection import *
+    # generates plot of cso reduction
+    cso_list = []
+    tot_vol_list = []
+    cso_reduction_list = []
+    numSims = results["numSims"] 
+    for i in range(0,numSims):
+        cso = calculate_cso(results["outflow_series"][i],ratio)
+        cso_list.append(cso["cso_volume"])
+        tot_vol_list.append(cso["tot_volume"])
+        #reduction = cso["tot_volume"] - cso["cso_volume"]
+        #reduction_list.append(reduction)
+    for i in range(0,numSims):
+        original_cso_volume = cso_list[0]  #cso from run with no lids
+        cso_reduction = original_cso_volume - cso_list[i]  
+        cso_reduction_list.append(cso_reduction)
+    print cso_reduction_list
+    return {"csoReduction": cso_reduction_list, "cso": cso_list, "totalVol": tot_vol_list}
---
+++ process_collection.py
@@ -0,0 +1,55 @@
+# IMPORTANT:  RUN THIS BLOCK FIRST
+# ONLY for loop tests for which all subcats have the same number of a single LID:
+from pymongo import MongoClient
+from process_collection import *
+def volume_reduction_and_runoff_volume_vs_lid_number(collectionName,numSubs, db):
+    from pymongo import MongoClient
+    from datetime import datetime
+    import matplotlib.pyplot as plt
+    mGal = 133680.5  # 1 Million gallon in cubic feet
+    runs = db[collectionName]
+    cursor = runs.find()
+    numLists = []
+    for n in range(0,numSubs):
+        numLists.append([])
+
+    volReductionList = []
+    totalSurfaceOutflowList = []
+    csoVolList=[]
+    noLidRun = cursor[0]
+    noLidVolume = noLidRun['volume']
+    outflow_series_list = []
+    for run in cursor:
+        for i in range(0,numSubs):
+            runParamsZero = run['runParamList'][i] 
+            numInitial = runParamsZero['Number']
+            numLists[i].append(numInitial)
+        volume = run['volume']
+        volReduction = noLidVolume-volume
+        volReductionList.append(volReduction)
+        outflow_series=run['outflow_series']
+        #outflow_series_list.append
+        series = run['outflow_series']
+        #print series
+        outflow_series_list.append(series)
+        if run['lidDict'] == None:
+            #numList.append(number)
+            totalSurfaceOutflowList.append(None)
+            continue
+        else:
+            totalSurfaceOutflow = 0.0
+            runParamList = run['runParamList']
+            lidDict = run['lidDict']
+            for lidUsage in runParamList: #was lidUsage in runParamList 
+                lid = lidUsage['LID']
+                subcat = lidUsage['Subcat']
+                lidNumber = lidUsage['Number']
+                lidArea = lidUsage['Area']  # in square feet
+                lidKey = subcat+' '+lid
+                surfaceOutflowInches = lidDict[lidKey]['Surface Outflow']
+                surfaceOutflow = surfaceOutflowInches*lidNumber*lidArea/12.0  # cubic feet
+                totalSurfaceOutflow += surfaceOutflow/mGal  # convert to million Gal/year
+            totalSurfaceOutflowList.append(totalSurfaceOutflow)
+    numSims = len(volReductionList)
+    return{"numLists": numLists, "outflow_series": outflow_series_list, "volReductionList": volReductionList, \
+           "surface_outflow":totalSurfaceOutflowList,"numSims": numSims}
